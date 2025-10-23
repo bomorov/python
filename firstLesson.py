863114a063b6254bca792c17af334c4de6db7ce6
bomorov <bomorov@it.salyk.kg>
2025-10-23 20:32:45 +0600

    firstLesson

--- firstLesson.py
+++ firstLesson.py
@@ -1,1 +1,7 @@
-print("Hello python")
+
+def question_method(question: str, answer: str):
+    input_value = f"{input(question)}"
+    print(f"{answer} {input_value}")
+
+question_method("What is your name? ", "Hello ")
+question_method("How old are you? ", "You are ") 
