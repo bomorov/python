59dcdb6d38d170a8c27e029707f8e5089fd9f663
Bekmyrza Omorov <bomorov1998@gmail.com>
2025-10-29 19:19:08 +0600

    Update fifth_lesson_home_work.py

--- fifth_lesson_home_work.py
+++ fifth_lesson_home_work.py
@@ -1,83 +1,114 @@
 import math as mt
 
-#Task1
-num = int(input("Enter a number: "))
-if num > 5:
-    print(True)
+# #Task1
+# num = int(input("Enter a number: "))
+# if num > 5:
+#     print(True)
+# else:
+#     print(False)
+
+
+# #Task2
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print(True)
+# else:
+#     print(False)
+
+# #Task3
+# a = int(input("Enter a : "))
+# b = int(input("Enter b : "))
+
+# if a == 2 * b : 
+#     print(True)
+# else:
+#     print(False)
+
+# #Task4
+# num = int(input("Enter a number: "))
+# if num % 3 == 0:
+#     print("Yes")
+# else:
+#     print("No")
+
+# #Task5
+# num = int(input("Enter a number: "))
+# if num % 2 != 0:
+#     print(num+1)
+# else:
+#     print(num)
+
+# #Task6
+# x = int(input("Enter a number: "))
+# if x > 3 and x <= 8:
+#     print("Yes")
+# else:
+#     print("No")
+
+# #Task7
+# x = int(input("Enter a number: "))
+# if x >= 5 and x < 15 and x != 10:
+#     print("Yes")
+# else:
+#     print("No")
+
+# #Task8
+# x = int(input("Enter a number: "))
+# if x <= 5 or x > 15:
+#     print("Yes")
+# else:
+#     print("No")
+
+# #Task9
+# x = int(input("Enter a number: "))
+# if (x > 2 and x <= 6) or x > 10:
+#     print("Yes")
+# else:
+#     print("No")
+
+# #Task10
+# x = int(input("Enter a number: "))
+# if (x < 4 and x > 10) or (x <= 2 and x >= 6):
+#     print("Yes")
+# else:
+#     print("No")
+
+# #Task11
+# x = int(input("Enter a number: "))
+# if (x > -1*float('inf') and x <= 3) or (x >= 5 and x < float('inf')):
+#     print("Yes")
+# else:
+#     print("No")
+
+
+# #Task12
+# x = int(input("Enter a number: "))
+# if (x < -3 and x <= 6):
+#     print("Yes")
+# elif (x >= 4 and x < float('inf')):
+#     print("Yes")
+# elif (x > -2 and x <= 3) or (x < 5 and x < float('inf')):
+#     print("Yes")
+# elif (x > 0 and x < 4) or (x <= 6 and x < 10):
+#     print("Yes")
+# else:
+#     print("No")
+
+# #Task13
+# x = int(input("Enter an angle: "))
+# if (x >= 40 and x <= 45):
+#     print("Opptimal Parameters")
+# elif (x < 40):
+#     print("Destroyed")
+# else:
+#     print("Impossible")
+
+# #Task14
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+if (y == 0.5 *x + 4):
+    print("On Line")
 else:
-    print(False)
-
-
-#Task2
-num = int(input("Enter a number: "))
-if num % 2 == 0:
-    print(True)
-else:
-    print(False)
-
-#Task3
-a = int(input("Enter a : "))
-b = int(input("Enter b : "))
-
-if a == 2 * b : 
-    print(True)
-else:
-    print(False)
-
-#Task4
-num = int(input("Enter a number: "))
-if num % 3 == 0:
-    print("Yes")
-else:
-    print("No")
-
-#Task5
-num = int(input("Enter a number: "))
-if num % 2 != 0:
-    print(num+1)
-else:
-    print(num)
-
-#Task6
-x = int(input("Enter a number: "))
-if x > 3 and x <= 8:
-    print("Yes")
-else:
-    print("No")
-
-#Task7
-x = int(input("Enter a number: "))
-if x >= 5 and x < 15 and x != 10:
-    print("Yes")
-else:
-    print("No")
-
-#Task8
-x = int(input("Enter a number: "))
-if x <= 5 or x > 15:
-    print("Yes")
-else:
-    print("No")
-
-#Task9
-x = int(input("Enter a number: "))
-if (x > 2 and x <= 6) or x > 10:
-    print("Yes")
-else:
-    print("No")
-
-#Task10
-x = int(input("Enter a number: "))
-if (x < 4 and x > 10) or (x <= 2 and x >= 6):
-    print("Yes")
-else:
-    print("No")
-
-#Task11
-x = int(input("Enter a number: "))
-if (x > -1*float('inf') and x <= 3) or (x >= 5 and x < float('inf')):
-    print("Yes")
-else:
-    print("No")
-
+    print("Out of Line")
     
